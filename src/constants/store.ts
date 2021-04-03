23c148bb10bce23e97a0438b65bfb6fd185a9a17
BokeChen <boke.chen@intellif.com>
2021-04-03 10:02:53 +0800

    feat(easy-peasy):easy-peasy 模块使用搭建相关文档

--- src/constants/store.ts
+++ src/constants/store.ts
@@ -1,5 +1,11 @@
 import { createStore } from 'easy-peasy';
-import { storeModel } from 'src/models/index';
+import { Middleware } from 'redux';
+import logger from 'redux-logger';
+import { storeModel } from 'src/models';
 
-const store = createStore(storeModel);
+const middlewares:Middleware[] = [];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+const store = createStore(storeModel, { middleware: middlewares });
 export default store;
